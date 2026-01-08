0d4ebfc26a7fb846c1e691560a10d6234bca47c2
litwor1 <rzepeckapaulina@gmail.com>
2026-01-08 20:33:47 +0100

    Support for adding and removing movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import "milligram";
+import MovieForm from "./MovieForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movies, setMovies] = useState([]);
+
+    const [showForm, setShowForm] = useState(false);
+
+    function deleteMovie(titleToDelete) {
+        const newMoviesList = movies.filter((movie) => movie.title !== titleToDelete);
+        setMovies(newMoviesList);
+    }
+
+    function handleAddMovie(movie) {
+        setMovies([...movies, movie]);
+        setShowForm(false);
+    }
+
+    return (
+        <div className="container">
+            <h1>My favourite movies to watch</h1>
+
+            <h2>Movies</h2>
+            {movies.length > 0 ? (
+                <ul>
+                    {movies.map((movie) => (
+                        <li key={movie.title}>
+                            {movie.title} ({movie.year})
+                            <a
+                                className="button-outline"
+                                style={{marginLeft: '10px'}}
+                                onClick={() => deleteMovie(movie.title)}>
+                                Delete
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No movies to display</p>
+            )}
+
+
+            {showForm ? (
+                <div style={{marginTop: '20px', borderTop: '1px solid #ccc', paddingTop: '20px'}}>
+
+                    <MovieForm onMovieSubmit={handleAddMovie}/>
+
+                </div>
+            ) : (
+                <button onClick={() => setShowForm(true)}>
+                    Add a movie
+                </button>
+            )}
+
+        </div>
+    );
 }
 
 export default App;
